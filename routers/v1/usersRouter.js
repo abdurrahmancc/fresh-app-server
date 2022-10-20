653bfe49761615ff20b53586460e411ad1d5c27d
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2022-10-21 03:37:31 +0600

    added google user api

--- routers/v1/usersRouter.js
+++ routers/v1/usersRouter.js
@@ -11,6 +11,7 @@ const {
   updateUserName,
   updatePresentAddress,
   updatePermanentAddress,
+  googleUser,
 } = require("../../Controller/usersController");
 const { verifyJWT, requireRole } = require("../../middleWares/common/checkLogin");
 
@@ -20,24 +21,11 @@ const {
 } = require("../../middleWares/users/userValidators");
 const router = express.Router();
 
-/**
- * @api {get} /tools All tools
- * @apiDescription Get all the tools
- * @apiPermission admin
- *
- * @apiHeader {String} Authorization   User's access token
- *
- * @apiParam  {Number{1-}}         [page=1]     List page
- * @apiParam  {Number{1-100}}      [limit=10]  Users per page
- *
- * @apiSuccess {Object[]} all the tools.
- *
- * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
- * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
- */
-
 /*---------- get user details------------*/
 router.get("/my-profile-details/:email", verifyJWT, myProfileDetails);
+
+/*---------- google user------------*/
+router.put("/google", googleUser);
 
 /*------------- update image --------------*/
 router.put("/update/photoURL/:email", verifyJWT, updateImage);
